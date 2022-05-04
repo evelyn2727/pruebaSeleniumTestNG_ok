50da06027a6c547ca2c5954b372a9a657010c77c
Milagros Lizárraga <evelyn2727@hotmail.com>
2022-05-03 22:29:08 -0500

    Update TestBase.java

--- src/main/java/base/TestBase.java
+++ src/main/java/base/TestBase.java
@@ -40,7 +40,7 @@ public class TestBase {
 		String browserName = prop.getProperty("browser");
 		
 		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", "resources/drivers/chrome/chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "resources/drivers/chrome/linux/chromedriver.exe");
 			driver = new ChromeDriver(); 
 		}
 		else if(browserName.equals("FF")){
